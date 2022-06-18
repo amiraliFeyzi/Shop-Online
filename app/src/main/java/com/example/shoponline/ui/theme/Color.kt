bf493d0f797358257e4c8310698c8d3b78f959b7
ASUS <amiralifeyz1382@gmail.com>
2022-06-18 15:01:22 +0430

    SetUp Theme -set theme project -add font in use project

--- app/src/main/java/com/example/shoponline/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoponline/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.example.shoponline.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Red = Color(0xFFFF0000)
+val BackGroundColor = Color(0xFFffffff)
+val Gray = Color(0xFF2c2c2c)
